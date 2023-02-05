2c38127c22095e070826cada2b3fdacebcf5a852
Саша Дудин <d.sasha.2021@gmail.com>
2023-02-06 01:06:44 +0300

    Removing the graph output function, working on adding data output in tabular format

--- nom1.py
+++ nom1.py
@@ -1,11 +1,8 @@
+from prettytable import PrettyTable
 import math
-import plotly
-import plotly.graph_objs as go
-import plotly.express as px
-from plotly import tools
-from plotly.subplots import make_subplots
-import numpy as np
-import pandas as pd
+
+days = []
+values = []
 
 # задание параметров
 def requirement(): #1
@@ -60,79 +57,37 @@ def maximum_desired_amount_of_stock(): #11
 def period_of_consumption_of_the_stock_up_to_the_threshold_level(): #12
 	param12 = math.ceil((maximum_desired_amount_of_stock() - stock_threshold_level()) / expected_daily_consumption())
 	return param12
-
- # проверка на достижение порогового уровня
-def threshold_level():
+	
+# достижение страхового уровня, пополнение запаса
+def safe_level():
 	stock_amount = maximum_desired_amount_of_stock() # 810
 	threshold_level = stock_threshold_level() # 810
 	daily_consumption = expected_daily_consumption() # 90
-	period_of_expenditure = period_of_expenditure_of_the_stock() # 8
+	stock_safety = safety_stock() # 180
+
+	for i in range(1, (time_of_shipment()+1)):
+		days.append(i)
+
+	# разобраться с циклами
 	if stock_amount == threshold_level:
-		print(stock_amount)	
 		print("Величина запаса достигла порогового уровня в " + str(stock_amount) + " единиц")
+		print("\n")
 	else:
 		while stock_amount > threshold_level:
 			stock_amount = stock_amount - daily_consumption
-			print(stock_amount)
 		print("Величина запаса достигла порогового уровня в " + str(stock_amount) + " единиц")
-	
-# достижение страхового уровня
-def safe_level():
-	stock_amount = maximum_desired_amount_of_stock() # 810
-	daily_consumption = expected_daily_consumption() # 90
-	stock_safety = safety_stock() # 180
+		print("\n")
+
 	if stock_amount == stock_safety:
-		print(stock_amount)
 		print("Величина запаса достигла страхового уровня в " + str(stock_amount) + " единиц")
+		print("\n")
 	else:
 		while stock_amount > stock_safety:
 			stock_amount = stock_amount - daily_consumption
-			print(stock_amount)	
+			values.append(stock_amount)
 		print("Величина запаса достигла страхового уровня в " + str(stock_amount) + " единиц")
+		print("\n")
 
-stock_amount = maximum_desired_amount_of_stock() # 810
-daily_consumption = expected_daily_consumption() # 90
-stock_safety = safety_stock() # 180
-TOS = time_of_shipment() # 7
+	print(days, values)
 
-x = np.arange(0, 8, 1)
-
-def f(x):
-	stock_amount = maximum_desired_amount_of_stock() # 810
-	daily_consumption = expected_daily_consumption() # 90
-	stock_safety = safety_stock() # 180	
-	return stock_amount - (daily_consumption*x)
-	stock_amount = stock_amount - daily_consumption
-	print(stock_amount)
-
-# создание поля графика
-fig = go.Figure()
-
-# задание размеров поля графика
-# параметры надо сделать зависимыми от величины запаса и количества дней расходования запаса
-fig.update_yaxes(range=[0, 900], zeroline=True, zerolinewidth=2, zerolinecolor='LightPink')
-fig.update_xaxes(range=[-10, 50], zeroline=True, zerolinewidth=2, zerolinecolor='#008000')
-
-# создание дополнительных линий
-fig.add_hline(y=maximum_desired_amount_of_stock(), line_dash="solid", line_color="#4682B4", line_width=7)
-fig.add_hline(y=stock_threshold_level(), line_dash="solid", line_color="#1be098", line_width=2)
-fig.add_hline(y=safety_stock(), line_dash="dash", line_color="#2547c2", line_width=2)
-
-# добавление "полос" графика
-fig.add_trace(go.Scatter(x=[x[0]], y=[f(x[0])], mode="lines+markers")) # trace 0
-
-# добавление точек графика
-frames = []
-for i in range(1, len(x)):
-	frames.append(go.Frame(data=[go.Scatter(x=x[:i+1], y=l(q[:i+1]))]))	# изменение trace 0
-fig.frames = frames
-
-# удаление отступов
-fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
-
-# отображение значений у точек
-fig.update_traces(hoverinfo="all", hovertemplate="День: %{x}<br>Величина запаса: %{y}")
-plotly.offline.plot(fig)          		
-
-print(f(x))
-
+safe_level()
